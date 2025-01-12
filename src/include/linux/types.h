72d0f514273a17f150835b54b51ccae8cf10a85f
Yijun <buglittlej@gmail.com>
2025-01-12 17:20:57 +0800

    add io functions

---
+++ src/include/linux/types.h
@@ -0,0 +1,13 @@
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+
+/* unsigned */
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+
+#endif
